c272a8e814d2dcc418e31b7b0135da1ac9da24dd
Arion49 <42716695+Arion49@users.noreply.github.com>
2018-09-07 11:20:57 -0300

    ;

--- verificacao_compra.php
+++ verificacao_compra.php
@@ -4,7 +4,7 @@ require 'openBD.php';
 session_start();
 
 if (isset($_SESSION["login"]) || isset($_SESSION["senha"])) {
-	header("Location: comprar.php")
+	header("Location: comprar.php");
 } else {
 	header("Location: cad.php");
 }
