6b717f4ec22fe93280e74acf901e95a244418e82
Gamerk5 <gamervicio66@gmail.com>
2018-09-10 15:43:20 -0300

    about 1.8

--- about.php
+++ about.php
@@ -1,207 +1,209 @@
  <!DOCTYPE html>
-  <html lang="pt-br">
+ <html lang="pt-br">
 
-      <meta charset="utf-8">
+ <meta charset="utf-8">
 
-      <title>Easy Supplies - Sobre nós</title>
+ <title>Easy Supplies - Sobre nós</title>
 
-      <link rel="stylesheet" type="text/css" href="css.css">
+ <link rel="stylesheet" type="text/css" href="css.css">
 
-      <link rel="stylesheet" href="animate/animate.css">
+ <link rel="stylesheet" href="animate/animate.css">
 
-      <!--Import Google Icon Font-->
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+ <!--Import Google Icon Font-->
+ <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-      <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
+ <!--Import materialize.css-->
+ <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
 
-      <!-- Compiled and minified CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
+ <!-- Compiled and minified CSS -->
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-        <!-- Icone do titulo -->
-        <link rel="shortcut icon" type="image/png" href="img/favicon.png">
+ <!-- Icone do titulo -->
+ <link rel="shortcut icon" type="image/png" href="img/favicon.png">
 
-        <meta name="description" value="Suprimentos fáceis, trazendo o melhor dos produtos orgânicos da forma mais fácil para você!">
+ <meta name="description" value="Suprimentos fáceis, trazendo o melhor dos produtos orgânicos da forma mais fácil para você!">
 
-        <link href="https://fonts.googleapis.com/css?family=Poppins|Slabo+27px" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css?family=Poppins|Slabo+27px" rel="stylesheet">
 
-        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+ <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 
-        <span id="background-img" 
-        style="
-        background-repeat: no-repeat;
-        background-position: center;
-        background-attachment: fixed;
-        z-index: -1;
-        position: fixed; 
-        max-height: 1170px;
-        height: 1170px;
-        width: 100%;
-        opacity: 0.3" class="animated infinite pulse slower">
-        </span>
+ <span id="background-img" 
+ style="
+ background-repeat: no-repeat;
+ background-position: center;
+ background-attachment: fixed;
+ z-index: -1;
+ position: fixed; 
+ max-height: 1170px;
+ height: 1170px;
+ width: 100%;
+ opacity: 0.3" class="animated infinite pulse slower">
+</span>
 
-        <body style="
-        background-color: rgba(111,51,155,0.8);
-        margin: 0;
-        padding: 0;">
+<body style="
+background-color: rgba(111,51,155,0.8);
+margin: 0;
+padding: 0;">
 
 
-          <?php
-            include_once("nav.php");
-          ?>
+<?php
+include_once("nav.php");
+?>
 
-   
-  
-   
+<div class="container-fluid width100" 
+style="margin-top: 50px;
+padding-top: 8px; 
+padding-bottom: 18px;
+margin-left: 9%; 
+margin-right: 9%
+" id="scss-cad">
 
 
-          <div class="container" style="margin-top: 50px;" id="scss-cad">
-    
+<div class="row"> <!-- EQUIPE -->
 
-            <div class="row"> <!-- EQUIPE -->
-       
 
-            
 
-              <div class="col s12 z-depth-4 animated fadeIn fast" style="margin-bottom: 30px; border-radius: 5px; background-color: #ededed;">
-            
-                <div class="row"> <!-- DIV PRETA-->
 
-                  <div class="col s12" style="text-align: center;">
-                    <h5 style="color: black; font-weight: bolder; font-family: 'Poppins', sans-serif;">Equipe</h5>
-                  </div>
-                </div> <!-- FIM DIV PRETA -->
+  <div class="col s12 z-depth-4 animated fadeIn fast" 
+  style="margin-bottom: 30px; 
+  background-color: #ededed;
+  ">
 
-            
+  <div class="row"> <!-- DIV PRETA-->
 
-                <div class="row"> <!-- ROW 1 -->
+    <div class="col s12" style="text-align: center;">
+      <h5 style="color: black; font-weight: bolder; font-family: 'Poppins', sans-serif;">Equipe</h5>
+    </div>
+  </div> <!-- FIM DIV PRETA -->
 
-                  <div class="col l6 m12 s12" style=""><!-- COL DA FOTO -->
 
-                    <div class="row"></div>
-                    
-                  <img class="responsive-img" src="img/back9.jpg" style="border-radius:15px; width:100%; height: 100%; " >
-               
 
+  <div class="row"> <!-- ROW 1 -->
 
+    <div class="col l6 m12 s12 valign-wrapper" style=""><!-- COL DA FOTO -->
 
-     
+      <img class="responsive-img z-depth-4" src="img/back9.jpg" 
+      style=" 
+      width:100%; 
+      height: 377px; 
+      padding: 0; 
+      margin: 0;
+      margin-top: 7px" >
 
-      
 
+    </div><!-- FIM DO COL DA FOTO -->
 
+    <div class="col l6 m12 s12"
+    ><!-- COL DA INFORMAÇÃO-->
 
 
-                  </div><!-- FIM DO COL DA FOTO -->
+    <ul class="collection z-depth-2" 
+    style="border-radius: 15px;
+    background-color: #3b1659;  "> 
+    <h6 style="font-weight: bold; text-align: center; color: white "> INTEGRANTES</h6>
 
-                  <div class="col l6 m12 s12"><!-- COL DA INFORMAÇÃO-->
-                      
-                    
-                    <ul class="collection z-depth-2" style="border-radius: 15px; background-color: #3b1659;  "> 
-                       <h6 style="font-weight: bold; text-align: center; color: white "> INTEGRANTES</h6>
+    <li class="collection-item avatar row"> <div class="col s11"> <img src="img/perfil.jpg" class="circle" height="40px" width="40px" alt="">
+      <span class="title">Árion Pereira Cerceau Netto</span></div>
+      <p> Programação / Revisão</p> 
+      <div class="col s1"> <i class="material-icons secondary-content" style="color: purple">grade</i> </div> </li> 
 
-                                      <li class="collection-item avatar row"> <div class="col s11"> <img src="img/perfil.jpg" class="circle" height="40px" width="40px" alt="">
-                                            <span class="title">Árion Pereira Cerceau Netto</span></div>
-                                            <p> Programação / Revisão</p> 
-                                           <div class="col s1"> <i class="material-icons secondary-content" style="color: purple">grade</i> </div> </li> 
 
-                                         
-                                      <li class="collection-item avatar row"><div class="col s11"> <img src="img/perfil.jpg" class="circle" height="40px" width="40px" alt="">
-                                            <span class="title">Belmario Presley Alves de Paula</span> </div>
-                                            <p> Documentação</p> 
-                                           <div class="col s1"><i class="material-icons secondary-content" style="color: purple;">grade</i> </div></li> 
+      <li class="collection-item avatar row"><div class="col s11"> <img src="img/perfil.jpg" class="circle" height="40px" width="40px" alt="">
+        <span class="title">Belmario Presley Alves de Paula</span> </div>
+        <p> Documentação</p> 
+        <div class="col s1"><i class="material-icons secondary-content" style="color: purple;">grade</i> </div></li> 
 
-                        
-                                      <li class="collection-item avatar row"><div class="col s11"><img src="img/perfil.jpg" class="circle" height="40px" width="40px" alt="">
-                                            <span class="title">Bruce Leon de Almeida</span> </div>
-                                            <p> Programação / Design</p>
-                                            <div class="col s1"><i class="material-icons secondary-content" style="color: purple">grade</i> </div> </li> 
 
-                        
-                                      <li class="collection-item avatar row"><div class="col s11"><img src="img/perfil.jpg" class="circle" height="40px" width="40px" alt="">
-                                            <span class="title">Gabriel Vitor Drumond Viana</span></div>
-                                            <p> Programação / Design</p>
-                                            <div class="col s1"><i class="material-icons secondary-content" style="color: purple">grade</i></div> </li> 
-                    </ul>
+        <li class="collection-item avatar row"><div class="col s11"><img src="img/perfil.jpg" class="circle" height="40px" width="40px" alt="">
+          <span class="title">Bruce Leon de Almeida</span> </div>
+          <p> Programação / Design</p>
+          <div class="col s1"><i class="material-icons secondary-content" style="color: purple">grade</i> </div> </li> 
 
 
+          <li class="collection-item avatar row"><div class="col s11"><img src="img/perfil.jpg" class="circle" height="40px" width="40px" alt="">
+            <span class="title">Gabriel Vitor Drumond Viana</span></div>
+            <p> Programação / Design</p>
+            <div class="col s1"><i class="material-icons secondary-content" style="color: purple">grade</i></div> </li> 
+          </ul>
 
 
-                  </div><!-- FIM DO COL DA INFORMAÇÃO -->
-                 
-                </div> <!-- FIM ROW 1-->
 
-              </div>
-            </div>  <!-- Fim EQUIPE -->
-            
 
+        </div><!-- FIM DO COL DA INFORMAÇÃO -->
 
+      </div> <!-- FIM ROW 1-->
 
-            <div class="row"> <!-- IDEIA -->        
+    </div>
+  </div>  <!-- Fim EQUIPE -->
 
-              <div class="col s12 z-depth-4" style="margin-bottom: 30px;  border-radius: 5px; background-color: #ededed;">
-                        
-                <div class="row"> <!-- DIV PRETA-->
-                  <div class="col m12 s12" style="text-align: center;">
-                    <h5 style="color: black; font-weight: bolder; font-family: 'Poppins', sans-serif;">Ideia</h5>
-                  </div>
-                </div> <!-- FIM DIV PRETA -->
-                        
-                <div class="row"> <!-- ROW 2 -->
 
-                  <div class="col l3 m12 s12 center-align" > <!-- COL DA FOTO -->
-                      
 
-                     
 
+  <div class="row"> <!-- IDEIA -->        
 
-                    <img class="responsive-img" src="img/lampada.png" style="width:auto; height:200px; " >
-                 
+    <div class="col s12 z-depth-4" style="margin-bottom: 30px; background-color: #ededed;">
 
-                  
+      <div class="row"> <!-- DIV PRETA-->
+        <div class="col m12 s12" style="text-align: center;">
+          <h5 style="color: black; font-weight: bolder; font-family: 'Poppins', sans-serif;">Ideia</h5>
+        </div>
+      </div> <!-- FIM DIV PRETA -->
 
+      <div class="row"> <!-- ROW 2 -->
 
+        <div class="col l3 m12 s12 center-align" > <!-- COL DA FOTO -->
 
 
-                  </div> <!-- FIM DO COL DA FOTO -->
 
-                  <div class="col l9 m12 s12"> <!-- COL DA INFORMAÇÃO-->
 
 
-                    <p style="text-align: justify; font-size: 18px; text-indent: 35px;">O interesse no tema foi pelo fato da área de produtos orgânicos ser pouco explorada e possuir pouca presença na sociedade brasileira (principalmente mineira), mas que se demonstra um movimento em expansão. Isso dá abertura para inovação dentro do segmento a partir da vinculação entre a entrega de novas experiências nesse tema, com a tecnologia que pode ser utilizada para promover a comodidade de receber em sua residência produtos mais saudáveis e confiáveis com poucos cliques.</p>
+          <img class="responsive-img" src="img/lampada.png" style="width:auto; height:200px; " >
 
 
 
 
 
 
-                  </div> <!-- FIM DO COL DA INFORMAÇÃO -->
 
-                </div> <!-- FIM ROW 2-->
+        </div> <!-- FIM DO COL DA FOTO -->
 
-              </div>
+        <div class="col l9 m12 s12"> <!-- COL DA INFORMAÇÃO-->
 
 
-            </div>  <!-- Fim IDEIA -->
+          <p style="text-align: justify; font-size: 18px; text-indent: 35px;">O interesse no tema foi pelo fato da área de produtos orgânicos ser pouco explorada e possuir pouca presença na sociedade brasileira (principalmente mineira), mas que se demonstra um movimento em expansão. Isso dá abertura para inovação dentro do segmento a partir da vinculação entre a entrega de novas experiências nesse tema, com a tecnologia que pode ser utilizada para promover a comodidade de receber em sua residência produtos mais saudáveis e confiáveis com poucos cliques.</p>
 
-          </div>
 
-        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
-      <script type="text/javascript" src="js.js"></script>
 
-      <?php
 
-        include_once ("footer.html");
 
-      ?>
+        </div> <!-- FIM DO COL DA INFORMAÇÃO -->
 
-    </body>
+      </div> <!-- FIM ROW 2-->
 
-    
-  </html>
+    </div>
+
+
+  </div>  <!-- Fim IDEIA -->
+
+</div>
+
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+<script type="text/javascript" src="js.js"></script>
+
+<?php
+
+include_once ("footer.html");
+
+?>
+
+</body>
+
+
+</html>
